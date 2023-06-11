a1cf7b2ac7af59340adcab188634e108255178d5
ShampaShaw <shampashaw088@gmail.com>
2023-06-11 20:10:56 +0530

    making schema

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+}).then(() => console.log("connected")) //if connected console.log connected
+.catch((err) => console.log(err)); //if not connected console.log err
 
 app.listen(5000, () => {
     console.log("Server is running");
