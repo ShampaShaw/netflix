0dd290f96bd75e0e54d2001f4cb2767197846496
ShampaShaw <shampashaw088@gmail.com>
2023-06-12 22:02:53 +0530

    delete method

--- routes/users.js
+++ routes/users.js
@@ -31,8 +31,47 @@ router.put("/:id", verify, async(req,res) => {
 })
 
 //DELETE
+
+router.delete("/:id", verify, async(req,res) => {
+    if( req.user?.id === req.params.id || req.user.isAdmin){  
+        try {                                               
+           await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("User has been deleted....!");
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json("You can delete your account");   //if we are not the admin or owner then send this error
+    }
+})
+
 //GET(GET ONE USER)
+
+router.get("/find/:id", async(req,res) => {
+   //anyone can reach the information of user so we don't need to verify the token
+        try {                                               
+           const user = await User.findById(req.params.id)
+           const { password, ...info } = user._doc
+            res.status(200).json(info);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+})
 //GET ALL(GET ALL USER)
+
+router.get("/", verify, async(req,res) => {
+    const query = req.query.new;
+    if(req.user.isAdmin){  
+        try {                                               
+           const users = query ? await User.find().limit(10) /*find only last 10 user*/ : await User.find() //get all users
+            res.status(200).json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed to get all users");  
+    }
+})
 //GET USER STATS (NO. OF USER PER MONTH)
 
 module.exports = router
