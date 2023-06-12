86255303d5ccecaf2c142b414d002c0df7dd3e37
ShampaShaw <shampashaw088@gmail.com>
2023-06-12 10:56:17 +0530

    verifyToken.js is created

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js"); //to update our password
+
+//UPDATE
+router.put("/:id", async(req,res) => {
+
+})
+
+//DELETE
+//GET(GET ONE USER)
+//GET ALL(GET ALL USER)
+//GET USER STATS (NO. OF USER PER MONTH)
+
