19ccfed152199dea17cc39c0402274a62f971e36
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2024-02-12 11:44:33 +0530

    changes

--- index.js
+++ index.js
@@ -12,13 +12,7 @@ const listRoutes = require("./routes/lists");
 
 dotenv.config();
 app.use(cors(
-    {
-        origin: "http://localhost:3000",
-        credentials: true
-    },{
-        origin: "http://localhost:4000",
-        credentials: true
-    }
+
 ));
 
 const PORT = process.env.PORT ;
